f2f43bf290e943e6674741aa251d811297f38721
Mahavir Prasad <83405683+mahavirprasad@users.noreply.github.com>
2023-05-20 16:08:13 +0530

    Add files via upload

---
+++ web-photo-ocr.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/process_image.py", methods=["POST"])
+def process_image():
+    image_file = request.files["image"]
+    image = Image.open(image_file)
+    text = pytesseract.image_to_string(image)
+
+    return text
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000)
